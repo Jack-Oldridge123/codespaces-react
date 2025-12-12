de9316ccb0a1573d082629309b859d205f681fc8
Jack-Oldridge123 <168638148+Jack-Oldridge123@users.noreply.github.com>
2025-12-12 14:32:20 +0000

    init commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <body className="Log-in">
+        <p className="small">
+          New Here?
+        </p>
+        <p className="BtnMenu">
+          Log In
+        </p>
+      </body>
+
+      <body className="Sign-in">
+        <p className="small">
+          Welcome Back!
+        </p>
+        <p className="BtnMenu">
+          Sign In
+        </p>
+      </body>
+
+      <header className="App-main">
+        <p>
+          Password Manager
+        </p>
+        <img src="FrontEnd/img/Lock.png" className="App-logo" alt="logo" />
+      </header>
+        
+        <p>
+          
+        </p>
+    </div>
+  );
+}
+
+export default App;
+
